397687f77dbeffa6c087d7ead68ce3d9927d53c5
vincenzobucaria <87119887+vincenzobucaria@users.noreply.github.com>
2024-02-10 15:48:36 +0100

    Update play_audio.py

--- play_audio.py
+++ play_audio.py
@@ -1,12 +1,1 @@
-from pydub import AudioSegment
-from pydub.playback import play
 
-
-
-def riproduci_rumore():
-    # for playing wav file
-    print("DEBUG: MAa")
-    song = AudioSegment.from_wav("white_noise.wav")
-    print('DEBUG: rumore bianco')
-    play(song)
-    
